b0205bc65c1e539a9fdfd3f80f41140d8d055978
Edgar Longat <64935800+longate58@users.noreply.github.com>
2020-12-22 18:41:01 -0400

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,6 +1,14 @@
-let myMap = L.map('myMap').setView([10.4749721, -66.5320073], 16)
-L.tileLayer(`https://c.tile.openstreetmap.org/{z}/{x}/{y}.png`, { //Fondo del mapa
-	maxZoom: 20,
+var mbAttr =
+   'Datos del Mapa &copy; Colaboradores de <a href="https://www.openstreetmap.org/">OpenStreetMap</a>, ' +
+   '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ';
+
+
+let myMap = L.map('myMap').setView([10.4769721, -66.5320073], 18)
+    var osm = L.tileLayer(`https://c.tile.osm.org/{z}/{x}/{y}.png`, { //Fondo del mapa
+	maxZoom: 50, attribution: 'Tiles &copy; OpenStreetMap &mdash; Copyright: &copy;2020 Long Art Digital',
+
+
+
 }).addTo(myMap);
 
     let iconMarker = L.icon({ //Agrega nuestro marcador
@@ -9,4 +17,32 @@ L.tileLayer(`https://c.tile.openstreetmap.org/{z}/{x}/{y}.png`, { //Fondo del ma
         iconAnchor: [30, 60]
 })
 
-let marker2 = L.marker([10.4769721, -66.5320073], { icon: iconMarker }).addTo(myMap)
+let marker2 = L.marker([10.4749721, -66.5320073], { icon: iconMarker }).addTo(myMap)
+
+            // Agrega la capa de visualizacion de las coordenadas 
+            // cuando se hace clic en el mapa 
+            // ---------------------------
+            var c = new L.Control.Coordinates();
+            c.addTo(map);
+
+            function onMapClick(e) {
+                c.setCoordinates(e);
+            }
+
+            map.on('click', onMapClick);
+            //--------------------------------
+
+
+
+//------------------------------------------------------
+    // Capas de superposicón agrupadas
+
+    var MapaBase = {
+      "Mapa base gris": gris,
+      "OpenStreetMap": osm,
+      "Topografía Base": esritopo,      
+      "Topografía Terreno": terreno,
+      "Topografía+Caminos": opentopomap,
+      "Topografía+Ríos": Esri_DeLorme_layer,
+      "Satelital": esri
+      }
